d89a32e2151067a07767a8007c12a649cbf09dc1
quanming <qmtan17@gmail.com>
2023-06-10 17:34:47 +0800

    update

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,3 +1,4 @@
+import AuthContext from './context/AuthContext'
 import './globals.css'
 
 export const metadata = {
@@ -8,7 +9,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <AuthContext>
+          {children}
+        </AuthContext>
+      </body>
     </html>
   )
 }
---
+++ app/signup/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Signup = () => {
+  return (
+    <div className='bg-primary min-h-screen'>Signup</div>
+  )
+}
+
+export default Signup
