69eadf14ae92e4c74a4e96ee78bedbf6a1b10afe
quanming <116425066+wahpiangle@users.noreply.github.com>
2023-09-16 14:28:47 +0800

    Update route.js

--- app/api/properties/route.js
+++ app/api/properties/route.js
@@ -3,33 +3,41 @@ import { NextResponse } from "next/server";
 
 export async function GET(req) {
     const listingType = req.nextUrl.searchParams.get('listingType')
-    switch(listingType) {
-        case 'rent':
-            return;
-            // TODO
-        case 'purchase':
-            return;
-            // TODO
-        default:
-            getProperties();
-    }
-    try {
-        const properties = await prisma.property.findMany({
-            where: {
-                listingType: 'Rent'
-            }
-        })
-        return new NextResponse(JSON.stringify(properties), { status: 200 });
-    } catch (error) {
-        console.log('Property Get Error: ', error)
-    }
+    return await getProperties(listingType)
 }
 
-async function getProperties(){
-    try{
-        const properties = await prisma.property.findMany();
-        return new NextResponse(JSON.stringify(properties), { status: 200 });
-    }catch(error){
-        console.log('Property Get Error: ', error)
+// ! in mongoDB listingType is saved as 'Rent' or 'Sale' but in the query it is 'rent' or 'purchase'
+async function getProperties(listingType){
+    switch(listingType){
+        case 'rent':
+            try{
+                const properties = await prisma.property.findMany({
+                    where:{
+                        listingType: 'Rent'
+                    }
+                });
+                return new NextResponse(JSON.stringify(properties), { status: 200 });
+            }catch(error){
+                console.log('Property Get Error: ', error)
+            }
+        case 'purchase':
+            try {
+                const properties = await prisma.property.findMany({
+                    where:{
+                        listingType: 'Sale'
+                    }
+                });
+                return new NextResponse(JSON.stringify(properties), { status: 200 });
+            } catch (error) {
+                console.log('Property Get Error: ', error)
+            }
+        default:
+            try{
+                const properties = await prisma.property.findMany();
+                return new NextResponse(JSON.stringify(properties), { status: 200 });
+            }catch(error){
+                console.log('Property Get Error: ', error)
+            }
     }
+
 }
